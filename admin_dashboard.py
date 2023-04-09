05225064fc3c135a29c94f581394d5080f4bad80
chirag <chirag42001@gmail.com>
2023-04-09 16:40:00 +0530

    admin login completed and integrated with dashboard

--- admin_dashboard.py
+++ admin_dashboard.py
@@ -19,16 +19,10 @@ def isAuth(x):
     else:
         return "YES"
 
-def toggle_button_unauth():
-    auth_button.lift()
-    # table_frame_auth.lift()
-
-def toggle_button_auth():
-    unauth_button.lift()
-    table_frame_unauth.lift()
-
 
 def do_auth():
+    global unauth_button
+    global auth_button
     global selected_row
     if selected_row is None:
         messagebox.showerror('error','Please select a record first')
@@ -38,9 +32,12 @@ def do_auth():
     c.execute(f''' UPDATE USERS SET authorized=1 WHERE id={selected_row[0]}''')
     db.commit()
     selected_row = None
+    unauth_button.lift()
     populate_table()
 
 def do_unauth():
+    global unauth_button
+    global auth_button
     global selected_row
     if selected_row is None:
         messagebox.showerror('error','Please select a record first')
@@ -50,10 +47,13 @@ def do_unauth():
     c.execute(f''' UPDATE USERS SET authorized=0 WHERE id={selected_row[0]}''')
     db.commit()
     selected_row = None
+    auth_button.lift()
     populate_table()
 
 
 def on_click(event):
+    global unauth_button
+    global auth_button
     global selected_row
     item = tree_unauth.identify('item', event.x, event.y)
     if not item:
@@ -71,6 +71,8 @@ def populate_table(x=None):
     global table_frame_unauth
     global table_label_unauth
     global tree_unauth
+    global unauth_button
+    global auth_button
 
     try:
         for r in tree_unauth.get_children():
@@ -113,149 +115,155 @@ def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
 
-global selected_row
-window = Tk()
-window.geometry("1440x900")
-window.configure(bg = "#FFFFFF")
-selected_row = None
+def dashboard():
+    global selected_row
+    global auth_button_image
+    global unauth_button_image
+    global image_image_2
+    global logout_button_image
+    global auth_button
+    global unauth_button
+    dash_window = Toplevel()
+    dash_window.geometry("1440x900")
+    dash_window.configure(bg = "#FFFFFF")
+    selected_row = None
 
-canvas = Canvas(
-    window,
-    bg = "#FFFFFF",
-    height = 900,
-    width = 1440,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
-canvas.place(x = 0, y = 0)
-
-
-canvas.create_text(
-    40.0,
-    30.0,
-    anchor="nw",
-    text="SecureLabs",
-    fill="#000000",
-    font=("Poppins SemiBold", 25 * -1)
-)
-canvas.create_text(
-    876.0,
-    102.0,
-    anchor="nw",
-    text="Admin Dashboard ",
-    fill="#000000",
-    font=("Poppins SemiBold", 35 * -1)
-)
+    canvas = Canvas(
+        dash_window,
+        bg = "#FFFFFF",
+        height = 900,
+        width = 1440,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge"
+    )
+    canvas.place(x = 0, y = 0)
 
 
-
-
-auth_button_image = PhotoImage(
-    file=relative_to_assets("button_1.png"))
-auth_button = Button(
-    image=auth_button_image,
-    borderwidth=0,
-    highlightthickness=0,
-    command=lambda: do_auth(),
-    # command=lambda: print("auth_button clicked"),
-    relief="flat"
-)
-auth_button.place(
-    x=73.0,
-    y=98.0,
-    width=212.0,
-    height=58.49334716796875
-)
-
-
-
-unauth_button_image = PhotoImage(
-    file=relative_to_assets("button_2.png"))
-unauth_button = Button(
-    image=unauth_button_image,
-    borderwidth=0,
-    highlightthickness=0,
-    command=lambda: do_unauth(),
-    # command=lambda: print("unauth_button clicked"),
-    relief="flat"
-)
-unauth_button.place(
-    x=73.0,
-    y=98.0,
-    width=212.0,
-    height=58.49334716796875
-)
+    canvas.create_text(
+        40.0,
+        30.0,
+        anchor="nw",
+        text="SecureLabs",
+        fill="#000000",
+        font=("Poppins SemiBold", 25 * -1)
+    )
+    canvas.create_text(
+        876.0,
+        102.0,
+        anchor="nw",
+        text="Admin Dashboard ",
+        fill="#000000",
+        font=("Poppins SemiBold", 35 * -1)
+    )
 
 
 
 
+    auth_button_image = PhotoImage(
+        file=relative_to_assets("button_1.png"))
+    auth_button = Button(dash_window,
+        image=auth_button_image,
+        borderwidth=0,
+        highlightthickness=0,
+        command=lambda: do_auth(),
+        # command=lambda: print("auth_button clicked"),
+        relief="flat"
+    )
+    auth_button.place(
+        x=73.0,
+        y=98.0,
+        width=212.0,
+        height=58.49334716796875
+    )
 
-logout_button_image = PhotoImage(
-    file=relative_to_assets("button_3.png"))
-logout_button = Button(
-    image=logout_button_image,
-    borderwidth=0,
-    highlightthickness=0,
-    command=lambda: window.destroy(),
-    relief="flat"
-)
-logout_button.place(
-    x=1268.0,
-    y=102.0,
-    width=127.0,
-    height=58.0
-)
 
 
-
-# update_button_image = PhotoImage(
-#     file=relative_to_assets("button_4.png"))
-# update_button = Button(
-#     image=update_button_image,
-#     borderwidth=0,
-#     highlightthickness=0,
-#     command=lambda: print("Update_button clicked"),
-#     relief="flat"
-# )
-# update_button.place(
-#     x=614.0,
-#     y=293.0,
-#     width=212.0,
-#     height=53.0
-# )
+    unauth_button_image = PhotoImage(
+        file=relative_to_assets("button_2.png"))
+    unauth_button = Button(dash_window,
+        image=unauth_button_image,
+        borderwidth=0,
+        highlightthickness=0,
+        command=lambda: do_unauth(),
+        # command=lambda: print("unauth_button clicked"),
+        relief="flat"
+    )
+    unauth_button.place(
+        x=73.0,
+        y=98.0,
+        width=212.0,
+        height=58.49334716796875
+    )
 
 
 
 
 
+    logout_button_image = PhotoImage(
+        file=relative_to_assets("button_3.png"))
+    logout_button = Button(dash_window,
+        image=logout_button_image,
+        borderwidth=0,
+        highlightthickness=0,
+        command=lambda: dash_window.destroy(),
+        relief="flat"
+    )
+    logout_button.place(
+        x=1268.0,
+        y=102.0,
+        width=127.0,
+        height=58.0
+    )
 
 
-image_image_2 = PhotoImage(
-    file=relative_to_assets("image_1.png"))
-image_2 = canvas.create_image(
-    720.0,
-    479.0,
-    image=image_image_2
-)
+
+    # update_button_image = PhotoImage(
+    #     file=relative_to_assets("button_4.png"))
+    # update_button = Button(
+    #     image=update_button_image,
+    #     borderwidth=0,
+    #     highlightthickness=0,
+    #     command=lambda: print("Update_button clicked"),
+    #     relief="flat"
+    # )
+    # update_button.place(
+    #     x=614.0,
+    #     y=293.0,
+    #     width=212.0,
+    #     height=53.0
+    # )
 
 
-global table_frame_unauth
-global table_label_unauth
-
-table_frame_unauth = Frame(window,width=1440,height=593)
-table_frame_unauth.pack_propagate(0)
-table_frame_unauth.place(x=0,y=183)
-
-table_label_unauth = Label(table_frame_unauth,width=1400,height=553)
-table_label_unauth.place(relx = 0.5,rely = 0.5,anchor=CENTER)
 
 
-global tree_unauth
-tree_unauth = ttk.Treeview(table_label_unauth)
-populate_table()
-tree_unauth.bind('<ButtonRelease-1>', on_click)
-window.resizable(False, False)
-window.mainloop()
 
+
+
+    image_image_2 = PhotoImage(
+        file=relative_to_assets("image_1.png"))
+    image_2 = canvas.create_image(
+        720.0,
+        479.0,
+        image=image_image_2
+    )
+
+
+    global table_frame_unauth
+    global table_label_unauth
+
+    table_frame_unauth = Frame(dash_window,width=1440,height=593)
+    table_frame_unauth.pack_propagate(0)
+    table_frame_unauth.place(x=0,y=183)
+
+    table_label_unauth = Label(table_frame_unauth,width=1400,height=553)
+    table_label_unauth.place(relx = 0.5,rely = 0.5,anchor=CENTER)
+
+
+    global tree_unauth
+    tree_unauth = ttk.Treeview(table_label_unauth)
+    populate_table()
+    tree_unauth.bind('<ButtonRelease-1>', on_click)
+    dash_window.resizable(False, False)
+    # window.mainloop()
 
--- admin_login.py
+++ admin_login.py
@@ -1,14 +1,50 @@
 
-
 from pathlib import Path
 
 # from tkinter import *
 # Explicit imports to satisfy Flake8
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
-
+from tkinter import messagebox
+import sqlite3
+import admin_dashboard as ad
 
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path(r"assets\frame11")
+
+
+
+
+def login_admin(details):
+    if details[0]=="" or details[1]=='':
+        messagebox.showerror('error',"No field should be left empty")
+        return
+    
+    db = sqlite3.connect("app_data/user_data.sqlite")
+    c = db.cursor()
+
+    c.execute('SELECT * FROM Admins')
+    rows = c.fetchall()
+
+    uname = details[1]
+    passw = details[0]
+
+    print(uname," ====== ",passw)
+    names = [row[1] for row in rows]
+    passwords = [row[2] for row in rows]
+
+    if uname not in names:
+        print("hellllloooo")
+        messagebox.showerror('error','username or password incorrect')
+        return
+    else:
+        idx = names.index(uname)
+        print("noooooooooooooooooooooooo")
+        if passw!=passwords[idx]:
+            messagebox.showerror('error','username or password is incorrect')
+            return
+        else:
+            ad.dashboard()
+ 
 
 
 def relative_to_assets(path: str) -> Path:
@@ -30,7 +66,6 @@ canvas = Canvas(
     highlightthickness = 0,
     relief = "ridge"
 )
-
 canvas.place(x = 0, y = 0)
 entry_image_1 = PhotoImage(
     file=relative_to_assets("entry_1.png"))
@@ -86,7 +121,7 @@ button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_1 clicked"),
+    command=lambda: login_admin([entry_1.get(),entry_2.get()]),
     relief="flat"
 )
 button_1.place(
@@ -96,6 +131,8 @@ button_1.place(
     height=53.0
 )
 
+
+
 canvas.create_text(
     112.0,
     254.0,
@@ -104,7 +141,6 @@ canvas.create_text(
     fill="#000000",
     font=("Poppins Medium", 30 * -1)
 )
-
 canvas.create_text(
     129.0,
     451.0,
@@ -113,7 +149,6 @@ canvas.create_text(
     fill="#000000",
     font=("Poppins Medium", 16 * -1)
 )
-
 canvas.create_text(
     129.0,
     359.0,
@@ -122,7 +157,6 @@ canvas.create_text(
     fill="#000000",
     font=("Poppins Medium", 16 * -1)
 )
-
 canvas.create_text(
     40.0,
     30.0,
@@ -131,5 +165,7 @@ canvas.create_text(
     fill="#000000",
     font=("Poppins SemiBold", 25 * -1)
 )
+
+
 window.resizable(False, False)
 window.mainloop()
